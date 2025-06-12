8a62ddfe483543827f8bad1529934b65ca9b48f7
juannpariente <jpariente000@gmail.com>
2025-06-12 05:48:40 -0300

    Add class stubs to Mindfulness activity

---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,35 @@
+public class ReflectionActivity : Activity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public ReflectionActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplatQuestions()
+    {
+        
+    }
+}
