02ab60d9b557806001ba84f58e449b6582e9a72c
juannpariente <jpariente000@gmail.com>
2025-06-16 16:26:55 -0300

    Complete Shapes program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("blue", 10);
+        Rectangle rectangle = new Rectangle("green", 10, 5.5);
+        Circle circle = new Circle("red", 5);
+        
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} | Area: {shape.GetArea()}");
+        }
     }
 }
